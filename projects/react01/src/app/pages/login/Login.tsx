78fda6cfe463f87a1b14f8052fd2b66b4be5cc49
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-05-06 08:26:14 -0300

    :zap: creating custom components

--- projects/react01/src/app/pages/login/Login.tsx
+++ projects/react01/src/app/pages/login/Login.tsx
@@ -1,7 +1,8 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from "react"
+import { InputLogin } from "./components/InputLogin"
 
 export const Login = () => {
-    const inputPasswordRef = useRef<HTMLInputElement>(null)
+    //const inputPasswordRef = useRef<HTMLInputElement>(null)
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
@@ -41,15 +42,24 @@ export const Login = () => {
 
             <p>Quantidade de caracteres no e-mail: {emailLenght}</p>
 
-            <label>
-                <span>Email</span>
-                <input value={email} onChange={e => setEmail(e.target.value)} onKeyDown={e => e.key === 'Enter' ? inputPasswordRef.current?.focus() : undefined}/>
-            </label>
+            <InputLogin 
+                label="Email"
+                value={email}
+                onChange={newValue => setEmail(newValue)}
+                //onPressEnter={() => inputPasswordRef.current?.focus()}
+            />
 
-            <label>
+            <InputLogin 
+                label="Senha"
+                type='password'
+                value={password}
+                onChange={newValue => setPassword(newValue)} 
+            />
+
+            {/* <label>
                 <span>Senha</span>
                 <input ref={inputPasswordRef} type='password' value={password} onChange={e => setPassword(e.target.value)}/>
-            </label>
+            </label> */}
 
             <button type="button" onClick={handleEntrar}>Entrar</button>
 
