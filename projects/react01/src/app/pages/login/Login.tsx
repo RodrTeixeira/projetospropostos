9169fcf09b780c9bb3763f30fe16cb8974c77364
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-05-07 09:00:20 -0300

    :zap: passing children to components

--- projects/react01/src/app/pages/login/Login.tsx
+++ projects/react01/src/app/pages/login/Login.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from "react"
+import { ButtonLogin } from "./components/ButtonLogin"
 import { InputLogin } from "./components/InputLogin"
 
 export const Login = () => {
@@ -54,7 +55,13 @@ export const Login = () => {
                 onChange={newValue => setPassword(newValue)} 
             />
 
-            <button type="button" onClick={handleEntrar}>Entrar</button>
+            <ButtonLogin type="button" onClick={handleEntrar}>
+                Entrar
+            </ButtonLogin>
+
+            {/* <ButtonLogin type="button" onClick={handleEntrar}>
+                Cadastrar-se
+            </ButtonLogin> */}
 
            </form>
         </div>
