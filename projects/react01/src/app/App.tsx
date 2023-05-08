259ba4637f72035f7a4956eb833f04ca48a96967
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-05-08 08:09:05 -0300

    :zap: using the context API

--- projects/react01/src/app/App.tsx
+++ projects/react01/src/app/App.tsx
@@ -1,9 +1,13 @@
 
 import { Routes } from "./routes"
+import { UsuarioLogadoProvider } from "./shared/contexts";
 
 export const App = () => {
   return (
+    <UsuarioLogadoProvider>
       <Routes />  
+    </UsuarioLogadoProvider>
+      
   );
 }
 
