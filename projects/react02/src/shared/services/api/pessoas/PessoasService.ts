c759cb44618039a88bef0b34a0e868e36063cf5d
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-06-11 08:55:33 -0300

    :zap: backend access services

--- projects/react02/src/shared/services/api/pessoas/PessoasService.ts
+++ projects/react02/src/shared/services/api/pessoas/PessoasService.ts
@@ -38,13 +38,53 @@ const getAll = async (page = 1, filter = ''): Promise<TPessoasComTotalCount | Er
     }
 };
 
-const getById = async (): Promise<any> => {};
+const getById = async (id: number): Promise<IDetalhePessoa | Error> => {
+    try {
+        
+        const { data } = await Api.get(`/pessoas/${id}`);
 
-const create = async (): Promise<any> => {};
+        if (data) {
+            return data;
+        }
+        return new Error('Erro ao consultar os registros.');
+    } catch (error) {
+        console.error(error);
+        return new Error((error as {message:string}).message || 'Erro ao consultar os registros.');
+    } 
+};
 
-const updateById = async (): Promise<any> => {};
+const create = async (dados: Omit<IDetalhePessoa, 'id'>): Promise<number | Error> => {
+    try {
+            
+        const { data } = await Api.post<IDetalhePessoa>('/pessoas', dados);
 
-const deleteById = async (): Promise<any> => {};
+        if (data) {
+            return data.id;
+        }
+        return new Error('Erro ao criar os registros.');
+    } catch (error) {
+        console.error(error);
+        return new Error((error as {message:string}).message || 'Erro ao criar os registros.');
+    }
+};
+
+const updateById = async (id: number, dados: IDetalhePessoa): Promise<void | Error> => {
+    try {          
+        await Api.put(`/pessoas/${id}`, dados);
+    } catch (error) {
+        console.error(error);
+        return new Error((error as {message:string}).message || 'Erro ao atualizar os registros.');
+    }
+};
+
+const deleteById = async (id: number): Promise<void | Error> => {
+    try {          
+        await Api.delete(`/pessoas/${id}`);
+    } catch (error) {
+        console.error(error);
+        return new Error((error as {message:string}).message || 'Erro ao apagar os registros.');
+    }
+};
 
 export const PessoasService = {
     getAll,
