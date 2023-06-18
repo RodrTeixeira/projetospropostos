230006399a093a63976db24ad549843c701e241d
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-06-18 08:37:24 -0300

    :zap: listing actions

--- projects/react02/src/pages/pessoas/ListagemDePessoas.tsx
+++ projects/react02/src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,6 +1,6 @@
-import { LinearProgress, Pagination, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from '@mui/material';
+import { Icon, IconButton, LinearProgress, Pagination, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from '@mui/material';
 import { useEffect, useMemo, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import { FerramentasDaListagem } from '../../shared/components';
 import { Environment } from '../../shared/environment';
@@ -12,6 +12,7 @@ import { IListagemPessoa, PessoasService } from '../../shared/services/api/pesso
 export const ListagemDePessoas: React.FC = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     const { debounce } = useDebounce();
+    const navigate = useNavigate();
 
     const [rows, setRows] = useState<IListagemPessoa[]>([]);
     const [totalCount, setTotalCount] = useState(0);
@@ -43,6 +44,24 @@ export const ListagemDePessoas: React.FC = () => {
         
     }, [busca, pagina]);
 
+    const handleDelete = (id: number) => {
+        
+        if (confirm('Realmente deseja apagar?')) {
+            PessoasService.deleteById(id)
+                .then(result => {
+                    if (result instanceof Error) {
+                        alert(result.message);
+                    } else {
+                        setRows(oldRows =>  [
+                            ...oldRows.filter(oldRow => oldRow.id !== id),
+                        ]);
+                        alert('Registro apagado com sucesso!!');
+                    }
+                });
+        }
+
+    };
+
     return (
         <LayoutBaseDePagina 
             titulo="Listagem de pessoas"
@@ -67,7 +86,14 @@ export const ListagemDePessoas: React.FC = () => {
                     <TableBody>
                         {rows.map(row => (
                             <TableRow key={row.id}>
-                                <TableCell>Ações</TableCell>
+                                <TableCell>
+                                    <IconButton size='small' onClick={() => handleDelete(row.id)}>
+                                        <Icon>delete</Icon>
+                                    </IconButton>
+                                    <IconButton size='small' onClick={() => navigate(`/pessoas/detalhe/${row.id}`)}>
+                                        <Icon>edit</Icon>
+                                    </IconButton>
+                                </TableCell>
                                 <TableCell>{row.nomeCompleto}</TableCell>
                                 <TableCell>{row.email}</TableCell>
                             </TableRow>
