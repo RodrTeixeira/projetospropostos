bb74feb6e9e38f6b49299700e225789453daacc0
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-06-22 08:13:00 -0300

    :zap: forms with unform

--- projects/react02/src/pages/pessoas/DetalheDePessoas.tsx
+++ projects/react02/src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,15 +1,24 @@
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { FerramentasDeDetalhe } from '../../shared/components';
 import { VTextField } from '../../shared/forms';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 
+interface IFormData {
+    email: string;
+    nomeCompleto: string;
+    cidadeId: string;
+}
+
 
 export const DetalheDePessoas: React.FC = () => {
     const { id = 'nova' } = useParams<'id'>();
     const navigate = useNavigate(); 
+
+    const formRef = useRef<FormHandles>(null);
 
     const [isLoading, setIsLoading] = useState(false);
     const [nome, setNome] = useState('');
@@ -31,8 +40,8 @@ export const DetalheDePessoas: React.FC = () => {
         }
     }, [id]);
 
-    const handleSave = () => {
-        console.log('Save');
+    const handleSave = (dados: IFormData) => {
+        console.log(dados);
     };
 
     const handleDelete = (id: number) => {
@@ -61,8 +70,8 @@ export const DetalheDePessoas: React.FC = () => {
                     mostrarBotaoApagar={id !== 'nova'}
                     mostrarBotaoNovo={id !== 'nova'}
 
-                    aoClicarEmSalvar={handleSave}
-                    aoClicarEmSalvarEFechar={handleSave}
+                    aoClicarEmSalvar={() => formRef.current?.submitForm()}
+                    aoClicarEmSalvarEFechar={() => formRef.current?.submitForm()}
                     aoClicarEmApagar={() => handleDelete(Number(id))}
                     aoClicarEmNovo={() => navigate('/pessoas/detalhe/nova')}
                     aoClicarEmVoltar={() => navigate('/pessoas')}
@@ -71,10 +80,10 @@ export const DetalheDePessoas: React.FC = () => {
             }
         >
         
-            <Form onSubmit={(dados) => console.log(dados)}>
+            <Form ref={formRef} onSubmit={handleSave}>
                 <VTextField name='nomeCompleto'/>
-                    
-                <button type='submit'>Submit</button>    
+                <VTextField name='email'/>
+                <VTextField name='cidadeId'/>   
             </Form>
            
         </LayoutBaseDePagina>
