55bb3ad8a8cd8eefaf3123bded79021242fc99f6
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-07-05 08:11:01 -0300

    :zap: developing the home page I

--- projects/react02/src/pages/dashboard/Dashboard.tsx
+++ projects/react02/src/pages/dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
-import { FerramentasDeDetalhe } from '../../shared/components';
+import { Box, Card, CardContent, Grid, Typography } from '@mui/material';
+import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 
 
@@ -6,11 +7,40 @@ export const Dashboard = () => {
     return (
         <LayoutBaseDePagina  
             titulo='Página inicial' 
-            barraDeFerramentas={(
-                <FerramentasDeDetalhe mostrarBotaoSalvarEFechar mostrarBotaoNovo />
-            )}
+            barraDeFerramentas={<FerramentasDaListagem mostrarBotaoNovo={false} />}
         >
-            Testando
+            <Box width='100%' display='flex'>
+                <Grid container margin={2}>
+                    <Grid item container spacing={2}>
+                        <Grid item xs={12} sm={12} md={6} lg={4} xl={2} >
+
+                            <Card>
+                                <CardContent>
+                                    <Typography variant='h5' align='center'>Total de Pessoas</Typography>
+                                    <Box padding={6} display='flex' justifyContent='center' alignItems='center' >
+                                        <Typography variant='h1'>10</Typography> 
+                                    </Box>
+                                </CardContent>
+                            </Card>
+
+                        </Grid>
+
+                        <Grid item xs={12} sm={12} md={6} lg={4} xl={2} >
+
+                            <Card>
+                                <CardContent>
+                                    <Typography variant='h5' align='center'>Total de Cidades</Typography>
+                                    <Box padding={6} display='flex' justifyContent='center' alignItems='center' >
+                                        <Typography variant='h1'>25</Typography> 
+                                    </Box>
+                                </CardContent>
+                            </Card>
+
+                        </Grid>
+
+                    </Grid>
+                </Grid>
+            </Box>
         </LayoutBaseDePagina>
     );
 };
